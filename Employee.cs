15d90281c23c6a8b6777b9e30ed001e63605e9b7
Susmitha-pillaveni <p.v.susmitha070199@gmail.com>
2021-06-03 12:01:10 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SeversideStateMgt
+{
+    public class Employee
+    {
+        public int Empid { get; set; }
+    }
+}
